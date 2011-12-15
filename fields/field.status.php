5a0ececed621fa8aca534d146686f776ee168d50
Giel Berkers <info@gielberkers.com>
2011-12-15 17:06:32 +0100

    - Publish label

--- fields/field.status.php
+++ fields/field.status.php
@@ -96,7 +96,7 @@ Class fieldStatus extends Field
 			$options[] = array(General::sanitize($v), false, $v);
 		}
 		
-		$label = Widget::Label($this->get('label'));
+		$label = Widget::Label($this->label());
 		$table = new XMLElement('table', null, array('class'=>'status'));
 		
 		// Show the headers:
